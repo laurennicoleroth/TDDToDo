06a49e2fa8457716154f961887b3567f241f6238
Lauren Roth <laurenroth@gmail.com>
2022-06-23 15:46:43 -0400

    Add and test checking items off using the done property and make then equatable with a uuid

--- TDDToDo/ToDoItem.swift
+++ TDDToDo/ToDoItem.swift
@@ -8,12 +8,19 @@
 import Foundation
 
 struct ToDoItem : Equatable {
+  let id: UUID
   let title : String
   let itemDescription : String?
   let timeStamp : TimeInterval?
   let location : Location?
+  var done : Bool = false
+  
+  static func == (lhs: ToDoItem, rhs: ToDoItem) -> Bool {
+    return lhs.id == rhs.id
+  }
   
   init(title: String, itemDescription: String? = nil, timeStamp: TimeInterval? = nil, location : Location? = nil) {
+    self.id = UUID()
     self.title = title
     self.itemDescription = itemDescription
     self.timeStamp = timeStamp
--- TDDToDoTests/ToDoItemStoreTests.swift
+++ TDDToDoTests/ToDoItemStoreTests.swift
@@ -28,39 +28,52 @@ class ToDoItemStoreTests: XCTestCase {
     
     XCTAssertEqual(receivedItems, [toDoItem])
   }
-}
-
-extension XCTestCase {
-  func wait<T: Publisher>(
-    for publisher: T,
-    afterChange change: () -> Void,
-    file: StaticString = #file,
-    line: UInt = #line) throws
-  -> T.Output where T.Failure == Never {
-    
-    let publisherExpectation = expectation(
-      description: "Wait for publisher in \(#file)"
-    )
-    
-    var result: T.Output?
-    
-    let token = publisher
-      .dropFirst()
-      .sink { value in
-        result = value
-        publisherExpectation.fulfill()
-      }
-    change()
-    
-    wait(for: [publisherExpectation], timeout: 1)
-    
-    token.cancel()
-    
-    let unwrappedResult = try XCTUnwrap(
-      result,
-      "Publisher did not publish any value"
-    )
-    
-    return unwrappedResult
+  
+  func test_check_shouldPublishChangeInDoneItems() throws {
+    let sut = ToDoItemStore()
+    let toDoItem = ToDoItem(title: "Dummy")
+    sut.add(toDoItem)
+    sut.add(ToDoItem(title: "Dummy 2"))
+    let receivedItems = try wait(for: sut.itemPublisher)
+    {
+      sut.check(toDoItem)
+    }
+    let doneItems = receivedItems.filter({ $0.done })
+    XCTAssertEqual(doneItems, [toDoItem])
   }
 }
+  
+  extension XCTestCase {
+    func wait<T: Publisher>(
+      for publisher: T,
+      afterChange change: () -> Void,
+      file: StaticString = #file,
+      line: UInt = #line) throws
+    -> T.Output where T.Failure == Never {
+      
+      let publisherExpectation = expectation(
+        description: "Wait for publisher in \(#file)"
+      )
+      
+      var result: T.Output?
+      
+      let token = publisher
+        .dropFirst()
+        .sink { value in
+          result = value
+          publisherExpectation.fulfill()
+        }
+      change()
+      
+      wait(for: [publisherExpectation], timeout: 1)
+      
+      token.cancel()
+      
+      let unwrappedResult = try XCTUnwrap(
+        result,
+        "Publisher did not publish any value"
+      )
+      
+      return unwrappedResult
+    }
+  }
