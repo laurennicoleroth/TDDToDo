786f97b7b64345b609e604d64015a930e53d034f
Lauren Roth <laurenroth@gmail.com>
2022-06-30 11:53:39 -0400

    Add core data store files

--- TDDToDo/TDDToDoApp.swift
+++ TDDToDo/TDDToDoApp.swift
@@ -9,12 +9,11 @@ import SwiftUI
 
 @main
 struct TDDToDoApp: App {
-    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environment(coreDataStore: CoreDataStore.default)
         }
     }
 }
